96cc4b6a8ab1d641e93fa823c6cf6a6ab9488337
Jack Blower <Jack@elvenspellmaker.co.uk>
2024-12-07 16:55:37 +0000

    feat: Day 7

---
+++ d7/d7p2.php
@@ -0,0 +1,35 @@
+<?php
+
+$line = array_map(fn(string $s) => explode(': ', $s), explode("\n", rtrim(file_get_contents('d7.txt'))));
+
+function performMaths(array $parts, int $pos, int $total, int $carry) : bool
+{
+	$newPos = $pos + 1;
+
+	if (! isset($parts[$newPos]))
+	{
+		return $carry === $total;
+	}
+
+	$one = $carry + $parts[$newPos];
+	$two = $carry * $parts[$newPos];
+	$three = $carry . $parts[$newPos];
+
+	return performMaths($parts, $newPos, $total, $one)
+		|| performMaths($parts, $newPos, $total, $two)
+		|| performMaths($parts, $newPos, $total, $three);
+}
+
+$sum = 0;
+
+foreach ($line as [$total, $parts])
+{
+	$parts = explode(' ', $parts);
+
+	if (performMaths($parts, 0, $total, $parts[0]))
+	{
+		$sum += $total;
+	}
+}
+
+echo $sum, "\n";
